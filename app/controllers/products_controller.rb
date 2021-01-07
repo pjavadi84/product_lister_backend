8d5c4d7b7e583f714a3fa0efca98c374d06d8aa0
pjavadi84 <pouya.javadi.resume@gmail.com>
2021-01-07 01:28:56 -0800

    adding CRUD for product controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,27 +1,37 @@
 class ProductsController < ApplicationController
     
     def index 
-
+        products = Product.all
     end
 
     def new 
-
+        product = Product.new
     end
 
     def create 
-
+        Product = Product.create(product_params)
+        render json: { products: products}
     end
 
     def edit 
-
+        product = Product.find(params[:id])
     end
 
     def update 
-
+        product - Product.find(params[:id])
+        product.update(product_params)
+        render json: { products: products }
     end
 
     def destroy 
+        product = Product.find(params[:id])
+        product.destroy
+        render json: { products: products }
+    end
 
+    private
+    def product_params
+        params.require(:product).permit(:name, :price, :quantity_on_hand, :description)
     end
 
 end
