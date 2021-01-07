74fe427715eb83059f30d3c76b520ccf1418af8f
pjavadi84 <pouya.javadi.resume@gmail.com>
2021-01-07 01:40:38 -0800

    configuring shopping_cart controller with built_in shopping cart session verification in application controller

--- app/controllers/shopping_carts_controller.rb
+++ app/controllers/shopping_carts_controller.rb
@@ -1,10 +1,13 @@
 class ShoppingCartsController < ApplicationController
     
     def show 
-
+        shopping_cart = @current_shopping_cart
     end
 
     def destroy 
-        
+        shopping_cart = @current_shopping_cart
+        shopping_cart.destroy
+        session[:shopping_cart_id] = nil
+        render json: { products: products}
     end
 end
